4cfa44526a387e662760cb3483e918976c1d1296
user zvC9 <user-zvC9@github.com>
2022-06-30 10:35:09 +0300

    added mkiso script (using xorrisofs)

---
+++ rootfs/bin/zvC9-mkiso-from-folder.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xorrisofs -o zvC9-mkiso.iso files-to-add-into-iso -iso-level 3 -U -allow-lowercase -l -r -J -joliet-long -V zvC9-mkiso  -d -max-iso9660-filenames 
+
+#-max-iso9660-filenames
+
